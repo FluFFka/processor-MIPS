e290864532081aacbbbb5ea1ae7b859624330513
FluFFka <iliasfedorenko@mail.ru>
2023-03-08 20:27:25 +0300

    Added some commands

--- include/funct.vh
+++ include/funct.vh
@@ -1,8 +1,11 @@
 `ifndef __FUNCT_VH
 `define __FUNCT_VH
 
-`define FUNCT_ADD 6'b100000
-`define FUNCT_SUB 6'b100010
-`define FUNCT_SLT 6'b101010
+`define FUNCT_ADD   6'b100000
+`define FUNCT_ADDU  6'b100001
+`define FUNCT_SUB   6'b100010
+`define FUNCT_SUBU  6'b100011
+`define FUNCT_SLT   6'b101010
+`define FUNCT_SLTU  6'b101011
 
 `endif // __FUNCT_VH
--- src/ALU.v
+++ src/ALU.v
@@ -7,12 +7,18 @@ module ALU (
         case (opcode)
             `OPCODE_R: begin
                 case (funct)
-                    `FUNCT_ADD: out <= $signed(in1) + $signed(in2); // signed and unsigned doesn't matter (?)
+                    `FUNCT_ADD:     out <= $signed(in1) + $signed(in2); // signed and unsigned doesn't matter (?)
+                    `FUNCT_ADDU:    out <= $unsigned(in1) + $unsigned(in2);
+                    `FUNCT_SUB:     out <= $signed(in1) - $signed(in2);
+                    `FUNCT_SUBU:    out <= $unsigned(in1) - $unsigned(in2);
+                    `FUNCT_SLT:     out <= ($signed(in1) < $signed(in2));
+                    `FUNCT_SLTU:    out <= ($unsigned(in1) < $unsigned(in2));
                 endcase
             end
-            `OPCODE_ADDIU: out <= $unsigned(in1) + $unsigned(in2);
-            `OPCODE_LW: out <= $signed(in1) + $signed(in2);   // calculate address
-            `OPCODE_SW: out <= $signed(in1) + $signed(in2);   // calculate address
+            `OPCODE_ADDI:   out <= $signed(in1) + $signed(in2);
+            `OPCODE_ADDIU:  out <= $unsigned(in1) + $unsigned(in2);
+            `OPCODE_LW:     out <= $signed(in1) + $signed(in2);   // calculate address
+            `OPCODE_SW:     out <= $signed(in1) + $signed(in2);   // calculate address
         endcase
     assign zero = out == 0;
 endmodule
