3b8d2785e84e660120e1f36e1646eaecec0b8522
FluFFka <iliasfedorenko@mail.ru>
2023-03-11 22:53:36 +0300

    Started pipeline processor

--- src/CPU.v
+++ src/CPU.v
@@ -7,22 +7,34 @@
 `include "src/RegisterSet.v"
 `include "src/ALU.v"
 
+`define NOOP    32'h00000000
+
 module CPU (
     input clk, input rst
 );
+
+    reg stall = 0;
+    wire stall_clk = !stall & clk;
+
     reg pc_load = 1;
     wire [31:0] pc_out;
     wire [31:0] pc_in;
     ProgramCounter PC(
-        .in(pc_in), .clk(clk), .rst(rst), .load(pc_load),
+        .in(pc_in), .clk(stall_clk), .rst(rst), .load(pc_load),
         .out(pc_out)
     );
 
+    wire [31:0] ir_out_f, ir_out_d, ir_out_e, ir_out_m, ir_out_w;
+    wire [31:0] ir_in_e = stall ? `NOOP : ir_out_d;
+    Register IR_D(.in(ir_out_f), .clk(stall_clk), .rst(rst), .load(1'b1), .out(ir_out_d));    
+    Register IR_E(.in(ir_in_e), .clk(clk), .rst(rst), .load(1'b1), .out(ir_out_e));    
+    Register IR_M(.in(ir_out_e), .clk(clk), .rst(rst), .load(1'b1), .out(ir_out_m));    
+    Register IR_W(.in(ir_out_m), .clk(clk), .rst(rst), .load(1'b1), .out(ir_out_w));  
 
-    wire [31:0] curr_instruction;
+
     InstructionMemory im(
         .addr(pc_out), .clk(clk),
-        .out(curr_instruction)
+        .out(ir_out_f)
     );
 
 
@@ -35,31 +47,69 @@ module CPU (
         .out(dm_out)
     );
 
+    wire [5:0] opcode_f = ir_out_f[31:26];
+    wire [4:0] rs_f = ir_out_f[25:21];
+    wire [4:0] rt_f = ir_out_f[20:16];
+    wire [4:0] rd_f = ir_out_f[15:11];
+    wire [4:0] shamt_f = ir_out_f[10:6];
+    wire [5:0] funct_f = ir_out_f[5:0];
+    wire [15:0] IMM_f = ir_out_f[15:0];
+    wire [31:0] EXT_IMM_f = $signed(IMM_f);
+    wire [25:0] ADDR_f = ir_out_f[25:0];
+    wire [31:0] EXT_ADDR_f = $unsigned(ADDR_f);
 
-    wire [5:0] opcode = curr_instruction[31:26];
-    // R-type
-    wire [4:0] rs = curr_instruction[25:21];    // also for I-type
-    wire [4:0] rt = curr_instruction[20:16];    // also for I-type
-    wire [4:0] rd = curr_instruction[15:11];
-    wire [4:0] shamt = curr_instruction[10:6];
-    wire [5:0] funct = curr_instruction[5:0];
-    // I-type
-    wire [15:0] IMM = curr_instruction[15:0];
-    wire [31:0] EXT_IMM = $signed(IMM);
-    // J-type
-    wire [25:0] ADDR = curr_instruction[25:0];
-    wire [31:0] EXT_ADDR = $unsigned(ADDR);
+    wire [5:0] opcode_d = ir_out_d[31:26];
+    wire [4:0] rs_d = ir_out_d[25:21];
+    wire [4:0] rt_d = ir_out_d[20:16];
+    wire [4:0] rd_d = ir_out_d[15:11];
+    wire [4:0] shamt_d = ir_out_d[10:6];
+    wire [5:0] funct_d = ir_out_d[5:0];
+    wire [15:0] IMM_d = ir_out_d[15:0];
+    wire [31:0] EXT_IMM_d = $signed(IMM_d);
+    wire [25:0] ADDR_d = ir_out_d[25:0];
+    wire [31:0] EXT_ADDR_d = $unsigned(ADDR_d);
+
+    wire [5:0] opcode_e = ir_out_e[31:26];
+    wire [4:0] rs_e = ir_out_e[25:21];
+    wire [4:0] rt_e = ir_out_e[20:16];
+    wire [4:0] rd_e = ir_out_e[15:11];
+    wire [4:0] shamt_e = ir_out_e[10:6];
+    wire [5:0] funct_e = ir_out_e[5:0];
+    wire [15:0] IMM_e = ir_out_e[15:0];
+    wire [31:0] EXT_IMM_e = $signed(IMM_e);
+    wire [25:0] ADDR_e = ir_out_e[25:0];
+    wire [31:0] EXT_ADDR_e = $unsigned(ADDR_e);
+
+    wire [5:0] opcode_m = ir_out_m[31:26];
+    wire [4:0] rs_m = ir_out_m[25:21];
+    wire [4:0] rt_m = ir_out_m[20:16];
+    wire [4:0] rd_m = ir_out_m[15:11];
+    wire [4:0] shamt_m = ir_out_m[10:6];
+    wire [5:0] funct_m = ir_out_m[5:0];
+    wire [15:0] IMM_m = ir_out_m[15:0];
+    wire [31:0] EXT_IMM_m = $signed(IMM_m);
+    wire [25:0] ADDR_m = ir_out_m[25:0];
+    wire [31:0] EXT_ADDR_m = $unsigned(ADDR_m);
+
+    wire [5:0] opcode_w = ir_out_w[31:26];
+    wire [4:0] rs_w = ir_out_w[25:21];
+    wire [4:0] rt_w = ir_out_w[20:16];
+    wire [4:0] rd_w = ir_out_w[15:11];
+    wire [4:0] shamt_w = ir_out_w[10:6];
+    wire [5:0] funct_w = ir_out_w[5:0];
+    wire [15:0] IMM_w = ir_out_w[15:0];
+    wire [31:0] EXT_IMM_w = $signed(IMM_w);
+    wire [25:0] ADDR_w = ir_out_w[25:0];
+    wire [31:0] EXT_ADDR_w = $unsigned(ADDR_w);
 
 
-    wire [4:0] rnum1;
-    wire [4:0] rnum2;
     wire [4:0] wnum;
     wire reg_write;
     wire [31:0] reg_wdata;
     wire [31:0] rdata1;
     wire [31:0] rdata2;
     RegisterSet registers(
-        .rnum1(rnum1), .rnum2(rnum2), .wnum(wnum),
+        .rnum1(rs_d), .rnum2(rt_d), .wnum(wnum),
         .clk(clk), .rst(rst), .write(reg_write), .wdata(reg_wdata),
         .rdata1(rdata1), .rdata2(rdata2)
     );
@@ -70,32 +120,73 @@ module CPU (
     wire [31:0] alu_in2;
     wire [31:0] alu_out;
     ALU alu(
-        .in1(alu_in1), .in2(alu_in2), .opcode(opcode), .funct(funct),
+        .in1(alu_in1), .in2(alu_in2), .opcode(opcode_e), .funct(funct_e),
         .out(alu_out), .zero(alu_zero)
     );
-    assign alu_in1 = rdata1;
-    assign alu_in2 = (opcode == `OPCODE_R) ? rdata2 : EXT_IMM;
 
 
-    assign rnum1 = rs;
-    assign rnum2 = rt;
-    assign wnum = (opcode == `OPCODE_R) ? rd : rt;
-    assign reg_write = !(opcode == `OPCODE_BEQ || opcode == `OPCODE_BNE ||
-                        opcode == `OPCODE_J || opcode == `OPCODE_SW);
-    assign reg_wdata = (opcode != `OPCODE_LW) ? alu_out : dm_out;
+    wire [31:0] pc_out_d;
+    Register pc_out_d_delayer(.in(pc_out), .clk(stall_clk), .rst(rst), .load(1'b1), .out(pc_out_d));
 
 
-    assign dm_addr = alu_out;
-    assign dm_in = rdata2;
-    assign dm_write = (opcode == `OPCODE_SW);
+    // assign alu_in1:
+    Register alu_in1_e_delayer(.in(rdata1), .clk(clk), .rst(rst), .load(1'b1), .out(alu_in1));
+    // assign alu_in2:
+    wire [31:0] alu_in2_before = (opcode_d == `OPCODE_R) ? rdata2 : EXT_IMM_d;
+    Register alu_in2_e_delayer(.in(alu_in2_before), .clk(clk), .rst(rst), .load(1'b1), .out(alu_in2));
+    wire [31:0] rdata2_e;
+    Register rdata2_e_delayer(.in(rdata2), .clk(clk), .rst(rst), .load(1'b1), .out(rdata2_e));
+    wire [31:0] rdata1_e;
+    Register rdata1_e_delayer(.in(rdata1), .clk(clk), .rst(rst), .load(1'b1), .out(rdata1_e));
+    wire [31:0] pc_out_e;
+    Register pc_out_e_delayer(.in(pc_out_d), .clk(clk), .rst(rst), .load(1'b1), .out(pc_out_e));
 
 
-    assign pc_in = (opcode == `OPCODE_J) ?
-                        EXT_ADDR : 
-                        ((opcode == `OPCODE_BEQ && rdata1 == rdata2) ||
-                         (opcode == `OPCODE_BNE && rdata1 != rdata2)) ? 
-                            $signed(pc_out) + $signed(EXT_IMM << 2) + 4 :
-                            pc_out + 4;
+    // assign dm_addr:
+    Register dm_addr_m_delayer(.in(alu_out), .clk(clk), .rst(rst), .load(1'b1), .out(dm_addr));
+    // assign dm_in:
+    Register rdata2_m_delayer(.in(rdata2_e), .clk(clk), .rst(rst), .load(1'b1), .out(dm_in));
+    assign dm_write = (opcode_m == `OPCODE_SW);
 
+
+    assign wnum = (opcode_w == `OPCODE_R) ? rd_w : rt_w;
+    assign reg_write = !(opcode_w == `OPCODE_BEQ || opcode_w == `OPCODE_BNE ||
+                        opcode_w == `OPCODE_J || opcode_w == `OPCODE_SW);
+    // assign reg_wdata:
+    wire [31:0] reg_wdata_before = (opcode_w != `OPCODE_LW) ? dm_addr : dm_out;
+    Register dm_addr_w_delayer(.in(reg_wdata_before), .clk(clk), .rst(rst), .load(1'b1), .out(reg_wdata));
+
+
+    assign pc_in = (opcode_f == `OPCODE_J) ?
+                        EXT_ADDR_f : 
+                        (opcode_e != `OPCODE_BEQ && opcode_e != `OPCODE_BEQ) ? pc_out + 4 :
+                        ((opcode_e == `OPCODE_BEQ && rdata1_e == rdata2_e) ||   // when fetching, need to calculate address - same time as ALU
+                         (opcode_e == `OPCODE_BNE && rdata1_e != rdata2_e)) ?   // when fetching, stall
+                            $signed(pc_out_e) + $signed(EXT_IMM_e << 2) + 4 :
+                            pc_out_e + 4;
+
+
+    wire read_rs = !(opcode_d == `OPCODE_J) && rs_d != 5'b00000;
+    wire read_rt = (opcode_d == `OPCODE_R || opcode_d == `OPCODE_SW ||
+                    opcode_d == `OPCODE_BEQ || opcode_d == `OPCODE_BNE) && rt_d != 5'b00000;    // zero register is always zero, no hazards with it
+    wire [4:0] write_num_e = (opcode_e == `OPCODE_R) ? rd_e : rt_e;
+    wire write_e = !(opcode_e == `OPCODE_BEQ || opcode_e == `OPCODE_BNE ||
+                    opcode_e == `OPCODE_J || opcode_e == `OPCODE_SW) && write_num_e != 5'b00000;
+    wire [4:0] write_num_m = (opcode_m == `OPCODE_R) ? rd_m : rt_m;
+    wire write_m = !(opcode_m == `OPCODE_BEQ || opcode_m == `OPCODE_BNE ||
+                    opcode_m == `OPCODE_J || opcode_m == `OPCODE_SW) && write_num_m != 5'b00000;
+    wire [4:0] write_num_w = (opcode_w == `OPCODE_R) ? rd_w : rt_w;
+    wire write_w = !(opcode_w == `OPCODE_BEQ || opcode_w == `OPCODE_BNE ||
+                    opcode_w == `OPCODE_J || opcode_w == `OPCODE_SW) && write_num_w != 5'b00000;
+
+
+    always @(negedge clk) begin
+        stall <= (read_rs && ((write_e && write_num_e == rs_d) || 
+                                (write_m && write_num_m == rs_d) ||
+                                (write_w && write_num_w == rs_d))) ||
+                   (read_rt && ((write_e && write_num_e == rt_d) || 
+                                (write_m && write_num_m == rt_d) ||
+                                (write_w && write_num_w == rt_d))); // ignoring branching
+    end
 
 endmodule
--- src/InstructionMemory.v
+++ src/InstructionMemory.v
@@ -6,7 +6,7 @@ module InstructionMemory #(
 );
     reg [7:0] memory [0:MEMORY_SIZE-1];
 
-    initial $readmemb("input/1.mem", memory);
+    initial $readmemb("input/2.mem", memory);
 
     assign out = (addr <= MEMORY_SIZE - 4) ? {memory[addr], memory[addr+1], memory[addr+2], memory[addr+3]} : 0;
 endmodule
